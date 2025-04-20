9a4d80694dbc9e67817dc433a53cd8a3bcee4285
rintan-r <dev.rintanrenatta@gmail.com>
2025-04-20 04:23:07 -0400

    add product and store data globally

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addItemToCart } from './CartSlice';
 
 const ProductList = () => {
 
@@ -9,11 +12,26 @@ const ProductList = () => {
     { id: 3, name: 'Product C', price: 30 },
   ];
 
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisableProducts] = useState([]); //State to store disabled products
+
+
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+        {products.map(product => (
+            <li key={product.id} className="product-list-item">
+            <span>{product.name} - ${product.price}</span>
+            <button
+                className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+                onClick={()=> handleAddToCart(product)}
+                disabled={disabledProducts.includes(product.id)} //Disable button if product is in disabledProd
+            >
+                Add to Cart
+            </button>
+            </li>
+        ))}
       </ul>
     </div>
   );
